1d620081d17c586eb418727d54c26d28969504cf
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2026-04-19 14:44:27 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import {Calculator} from './Calculator.jsx'
+
+export const App = () => {
+  return (
+    <div className="App">
+        <Calculator />
+    </div>
+  );
+}
---
+++ src/Calculator.jsx
@@ -0,0 +1,19 @@
+import { useState, useMemo } from "react"
+
+
+const numbers = Array.from({"length": 10000}, (_, i) => i + 1);
+
+export const Calculator = () => {
+    const [number, setNumber] = useState(0)
+
+    const Sum = useMemo(() => {return numbers.reduce((acc, number) => {return acc + number}, 0);}, [numbers]);
+
+
+    console.log(numbers)
+
+    return (
+        <div className="Calc">
+            <p>{Sum()}</p>
+        </div>
+    )
+}
